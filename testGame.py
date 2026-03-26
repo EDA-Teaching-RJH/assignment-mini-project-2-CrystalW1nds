dd7bf3e16240887dac9a94d7018b95b800ca0980
CrystalW1nds <hf280@kent.ac.uk>
2026-03-26 15:28:02 +0000

    added testing files and adjusted based on results

---
+++ testGame.py
@@ -0,0 +1,50 @@
+import os
+import unittest
+import csv
+from game import Weapon, PlayerCharacter, EnemyNPC, loadCharacter, writeCharacterToCSV
+from validators import validClass, validName, validRace
+
+class TestGame(unittest.TestCase):
+    def test_isAlive(self):
+        weapon = Weapon("Sword", 10, 10, "melee")
+        player = PlayerCharacter("TestPlayer", 100, 100, 100, "warrior", "human", weapon)
+        self.assertTrue(player.isAlive())
+
+    def test_takeDamage(self):
+        weapon = Weapon("Sword", 10, 10, "melee")
+        player = PlayerCharacter("TestPlayer", 100, 100, 100, "warrior", "human", weapon)
+        player.takeDamage(50)
+        self.assertEqual(player.health, 50)
+
+    def test_use(self):
+        weapon = Weapon("Sword", 10, 10, "melee")
+        player = PlayerCharacter("TestPlayer", 100, 100, 100, "warrior", "human", weapon)
+        target = EnemyNPC("TestTarget", 100, 100, 100, "warrior", "human", weapon)
+        weapon.use(target)
+        self.assertEqual(target.health, 90)
+
+    def test_validName(self):
+        self.assertTrue(validName("Henry"))
+        self.assertFalse(validName("Henry223"))
+
+    def test_validClass(self):
+        self.assertTrue(validClass("warrior"))
+        self.assertFalse(validClass("archer"))
+
+    def test_validRace(self):
+        self.assertTrue(validRace("elf"))
+        self.assertFalse(validRace("dwarf"))
+
+    def test_saveAndLoadCharacter(self):
+        weapon = Weapon("Sword", 10, 10, "melee")
+        player = PlayerCharacter("TestPlayer", 100, 100, 100, "warrior", "human", weapon)
+        filename = "test_characterSheet.csv"
+
+        writeCharacterToCSV(player, filename)
+        loaded = loadCharacter(filename)
+
+        self.assertEqual(loaded.name, "TestPlayer")
+        self.assertEqual(loaded.weapon.name, "Sword")
+
+if __name__ == '__main__':
+    unittest.main()
--- validators.py
+++ validators.py
@@ -1,13 +1,19 @@
 import re
 
 def validName(name):
-    if re.fullmatch(r"[A-Za-z]", name):
+    if re.fullmatch(r"[A-Za-z]{2,12}", name):
         return True
+    else:
+        return False
     
 def validClass(classs):
     if re.fullmatch(r"(warrior|mage)", classs):
         return True
+    else:        
+        return False
     
 def validRace(race):
     if re.fullmatch(r"(human|elf|troll|orc)", race):
         return True
+    else:
+        return False
